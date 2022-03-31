ad8f76d2741c29162f138fb79632d57e1af018e2
Andriy Kryvopysha <logan1kry@gmail.com>
2022-03-31 15:29:31 +0300

    Update render_gallery.js
    use hbs as Tpl

--- src/js/render_gallery.js
+++ src/js/render_gallery.js
@@ -1,3 +1,5 @@
+import cardTpl from '../templates/card_template.hbs'
+
 export { renderGallery };
 
 const gallery = document.querySelector('.gallery');
@@ -5,20 +7,8 @@ const gallery = document.querySelector('.gallery');
 function renderGallery(images) {
   const markup = images
     .map(image => {
-      const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
-      return `
-        <a class="gallery__link" href="${largeImageURL}">
-          <div class="gallery-item" id="${id}">
-            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item"><b>Likes</b>${likes}</p>
-              <p class="info-item"><b>Views</b>${views}</p>
-              <p class="info-item"><b>Comments</b>${comments}</p>
-              <p class="info-item"><b>Downloads</b>${downloads}</p>
-            </div>
-          </div>
-        </a>
-      `;
+           
+      return cardTpl(image);      
     })
     .join('');
 
